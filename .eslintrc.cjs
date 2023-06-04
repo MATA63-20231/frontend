582e2f14e7e9c280037dba23146419a443679988
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-04 19:10:07 -0300

    setup lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,29 @@
 module.exports = {
-    env: { browser: true, es2020: true },
+    env: { browser: true, es2021: true },
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
+        "airbnb",
+        "airbnb/hooks",
+        "plugin:react/recommended",
         "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended",
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    plugins: ["react-refresh"],
+    plugins: [
+        "react",
+        "react-refresh",
+        "react-hooks",
+        "prettier",
+        "@typescript-eslint",
+    ],
     rules: {
         "react-refresh/only-export-components": "warn",
         indent: ["error", 2],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        camelcase: "error",
+        "no-duplicate-imports": "error",
     },
 };
