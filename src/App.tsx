f7d868ac191cd1b068460f17c40d58559669e8b5
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-05 17:43:38 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid/Grid";
 import createTheme from "@mui/material/styles/createTheme";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import RecipeCreation from "./pages/RecipeCreation.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RecipeCreation />,
+  },
+  {
+    path: "/a",
+    element: <div>oi</div>,
+  },
+]);
 
 function App() {
   const defaultTheme = createTheme();
@@ -16,7 +28,7 @@ function App() {
       <Header />
 
       <Grid sx={{ minHeight: "80vh", bgcolor: "#212529" }}>
-        <RecipeCreation />
+        <RouterProvider router={router} />
       </Grid>
 
       <Footer />
