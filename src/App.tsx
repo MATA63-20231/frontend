22ec7350cddbf30588f64499d96ec396035c637c
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-05 18:50:56 -0300

    sei la

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid/Grid";
-import createTheme from "@mui/material/styles/createTheme";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/Header.tsx";
-import Footer from "./components/Footer.tsx";
 import RecipeCreation from "./pages/RecipeCreation.tsx";
+import AAAAAAAAAAAA from "./components/AAAAAAAAAAAA.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RecipeCreation />,
-  },
-  {
-    path: "/a",
-    element: <div>oi</div>,
-  },
+    {
+        element: <AAAAAAAAAAAA />,
+        children: [
+            {
+                path: "/",
+                element: <RecipeCreation />,
+            },
+            {
+                path: "/a",
+                element: <div>oi</div>,
+            },
+        ],
+    },
 ]);
 
 function App() {
-  const defaultTheme = createTheme();
-
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-
-      <Header />
-
-      <Grid sx={{ minHeight: "80vh", bgcolor: "#212529" }}>
-        <RouterProvider router={router} />
-      </Grid>
-
-      <Footer />
-    </ThemeProvider>
-  );
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
