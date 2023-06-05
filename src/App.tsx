10709c9f11eae6328bb832338e9eb9025ba5fc38
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2023-06-05 16:46:47 -0300

    header e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "../public/vite.svg";
-import "./App.css";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid/Grid";
+import createTheme from "@mui/material/styles/createTheme";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Blog from "./Blog";
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const defaultTheme = createTheme();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((c) => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+
+      <Header />
+
+      <Grid sx={{ minHeight: "80vh", bgcolor: "#212529" }}>
+        <Blog /> 
+      </Grid>
+
+      <Footer />
+    </ThemeProvider>
   );
 }
 
---
+++ src/Footer.tsx
@@ -0,0 +1,34 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+
+export default function Footer() {
+    return (
+        <Box component="footer" sx={{ bgcolor: "background.paper", py: 6 }}>
+            <Container maxWidth="lg">
+                <Typography variant="h6" align="center" gutterBottom>
+                    Chef Virtual
+                </Typography>
+                <Typography
+                    variant="subtitle1"
+                    align="center"
+                    color="text.secondary"
+                    component="p">
+                    Receitas para curtir com a família!
+                </Typography>
+                <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    align="center">
+                    {"Copyright © "}
+                    <Link color="inherit" href="/">
+                        Chef Virtual
+                    </Link>{" "}
+                    {new Date().getFullYear()}
+                    {"."}
+                </Typography>
+            </Container>
+        </Box>
+    );
+}
