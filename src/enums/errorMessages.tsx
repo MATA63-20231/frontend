6970de27e699b1b196a914a368a2ea8a0977fa3f
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-06 01:13:40 -0300

    form v2

--- src/enums/errorMessages.tsx
+++ src/enums/errorMessages.tsx
@@ -4,7 +4,7 @@ export enum FormErrorMessages {
   INVALID_EMAIL = "E-mail inválido.",
 
   // Mínimo de caractéres
-  MIN_2 = "Deve ter pelo menos 2 caractéres.",
+  MIN_CHAR_2 = "Deve ter pelo menos 2 caractéres.",
 
   // Máximo de caractéres
   MAX_255 = "Deve ter no máximo 255 caractéres.",
--- src/pages/RecipeCreation/components/RecipeCreationForm.tsx
+++ src/pages/RecipeCreation/components/RecipeCreationForm.tsx
@@ -1,42 +1,76 @@
 import * as Yup from "yup";
 import Button from "@mui/material/Button";
-import { Formik, Form, Field } from "formik";
+import Typography from "@mui/material/Typography";
+import { Formik, Form, Field, FormikHelpers } from "formik";
 import { TextField } from "formik-mui";
 import FormErrorMessages from "../../../enums/errorMessages";
+import { useState } from "react";
+
+interface IFields {
+  recipeTitle: string;
+  ingredients: string[];
+  directions: string[];
+}
 
 const RecipeCreationSchema = Yup.object().shape({
   recipeTitle: Yup.string()
-    .min(2, FormErrorMessages.MIN_2)
+    .min(2, FormErrorMessages.MIN_CHAR_2)
     .max(255, FormErrorMessages.MAX_255)
     .required(FormErrorMessages.REQUIRED),
 
-  ingredient: Yup.string()
-    .min(2, FormErrorMessages.MIN_2)
-    .max(255, FormErrorMessages.MAX_255)
+  ingredients: Yup.array()
+    .of(
+      Yup.string()
+        .min(2, FormErrorMessages.MIN_CHAR_2)
+        .max(255, FormErrorMessages.MAX_255)
+        .required(FormErrorMessages.REQUIRED)
+    )
     .required(FormErrorMessages.REQUIRED),
 
-  directions: Yup.string()
-    .min(2, FormErrorMessages.MIN_2)
-    .max(255, FormErrorMessages.MAX_255)
+  directions: Yup.array()
+    .of(
+      Yup.string()
+        .min(2, FormErrorMessages.MIN_CHAR_2)
+        .max(255, FormErrorMessages.MAX_255)
+        .required(FormErrorMessages.REQUIRED)
+    )
     .required(FormErrorMessages.REQUIRED),
 });
 
 export default function RecipeCreationForm() {
+  const [ingredients, setIngredients] = useState<string[]>([""]);
+  const [directions, setDirections] = useState<string[]>([""]);
+
+  const initialValues = {
+    recipeTitle: "",
+    ingredients,
+    directions,
+  };
+
+  const addIngredient = () => {
+    setIngredients([...ingredients, ""]);
+  };
+
+  const addStep = () => {
+    setDirections([...directions, ""]);
+  };
+
+  const onSubmit = (
+    values: IFields,
+    { setSubmitting }: FormikHelpers<IFields>
+  ) => {
+    console.log(values);
+    setTimeout(() => {
+      setSubmitting(false);
+      alert(JSON.stringify(values, null, 2));
+    }, 500);
+  };
+
   return (
     <Formik
-      initialValues={{
-        recipeTitle: "",
-        ingredient: "",
-        directions: "",
-      }}
+      initialValues={initialValues}
       validationSchema={RecipeCreationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        setTimeout(() => {
-          setSubmitting(false);
-          alert(JSON.stringify(values, null, 2));
-        }, 500);
-      }}>
+      onSubmit={onSubmit}>
       {({ submitForm, isSubmitting }) => (
         <Form>
           <Field
@@ -46,28 +80,33 @@ export default function RecipeCreationForm() {
             type="text"
             label="Título da Receita"
           />
-
-          <Field
-            component={TextField}
-            fullWidth
-            name="ingredient"
-            type="text"
-            label="Ingredientes"
-          />
-
-          <Field
-            component={TextField}
-            fullWidth
-            name="directions"
-            type="text"
-            label="Modo de Preparo"
-          />
-
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={isSubmitting}
-            onClick={submitForm}>
+          <Typography> Ingredientes</Typography>
+          {ingredients.map((_, index) => (
+            <Field
+              component={TextField}
+              size="small"
+              fullWidth
+              name={`ingredients[${index}]`}
+              type="text"
+            />
+          ))}
+          <Button onClick={addIngredient}>Adicionar ingrediente</Button>
+          <br />
+          <br />
+          <Typography> Modo de Preparo</Typography>
+          {directions.map((_, index) => (
+            <Field
+              component={TextField}
+              size="small"
+              fullWidth
+              name={`directions[${index}]`}
+              type="text"
+            />
+          ))}
+          <Button onClick={addStep}>Adicionar passo</Button>
+          <br />
+          <br />
+          <Button color="primary" disabled={isSubmitting} onClick={submitForm}>
             Submit
           </Button>
         </Form>
