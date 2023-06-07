666d5971e796056854c9e1449afc1895e34c6fbd
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-07 00:56:38 -0300

    lista responsiva

--- src/pages/RecipeView.tsx
+++ src/pages/RecipeView.tsx
@@ -2,82 +2,127 @@ import CardMedia from "@mui/material/CardMedia";
 import Page from "../components/Page.tsx";
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem/ListItem";
-
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
 import TimerOutlinedIcon from "@mui/icons-material/TimerOutlined";
 import RamenDiningOutlinedIcon from "@mui/icons-material/RamenDiningOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
 
+// TODO: Componentizar rs
 export default function RecipeView() {
   return (
     <Page title="Título da Receita">
-      <Grid>
-        <Card sx={{ width: "60%", m: "auto" }}>
+      <Grid container sx={{ m: "0 auto", maxWidth: "700px" }}>
+        <Card>
           <CardMedia
             component="img"
             image="https://source.unsplash.com/random?wallpapers"
           />
           <Stack
             direction={{ xs: "column", md: "row" }}
-            spacing={{ xs: 1, sm: 2, md: 4 }}
-            justifyContent="center"
-            alignItems="center"
-            sx={{ minWidth: 0 }}
-          >
-            <Grid item>
-              <Grid container sx={{ flexWrap: "nowrap" }}>
-                <TimerOutlinedIcon
-                  fontSize="small"
-                  color="primary"
-                  sx={{ mr: 0.5 }}
-                />
-                <Typography noWrap sx={{ maxWidth: "100%" }}>
-                  1h30min
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container sx={{ flexWrap: "nowrap" }}>
-                <RamenDiningOutlinedIcon
-                  fontSize="small"
-                  color="primary"
-                  sx={{ mr: 0.5 }}
-                />
-                <Typography noWrap sx={{ maxWidth: "100%" }}>
-                  40 porções
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container sx={{ flexWrap: "nowrap" }}>
-                <PersonOutlineOutlinedIcon
-                  fontSize="small"
-                  color="primary"
-                  sx={{ mr: 0.5 }}
-                />
-                <Typography noWrap sx={{ maxWidth: "100%" }}>
-                  Beatriz Cerqueira
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container sx={{ flexWrap: "nowrap" }}>
-                <CalendarMonthOutlinedIcon
-                  fontSize="small"
-                  color="primary"
-                  sx={{ mr: 0.5 }}
-                />
-                <Typography noWrap sx={{ maxWidth: "100%" }}>
-                  06/06/2023
-                </Typography>
-              </Grid>
-            </Grid>
+            spacing={{ xs: 0.5, md: 1 }}
+            sx={{
+              px: 1,
+              py: 2,
+            }}>
+            <Stack
+              direction="row"
+              spacing={0.5}
+              sx={{
+                flexWrap: "nowrap",
+                alignItems: "center",
+                justifyContent: "center",
+                minWidth: 0,
+                width: "100%",
+              }}>
+              <TimerOutlinedIcon fontSize="small" color="primary" />
+              <Typography
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}>
+                1h30min
+              </Typography>
+            </Stack>
+            <Divider
+              orientation="vertical"
+              sx={{ borderColor: "primary.light" }}
+              flexItem
+            />
+            <Stack
+              direction="row"
+              spacing={0.5}
+              sx={{
+                flexWrap: "nowrap",
+                alignItems: "center",
+                justifyContent: "center",
+                minWidth: 0,
+                width: "100%",
+              }}>
+              <RamenDiningOutlinedIcon fontSize="small" color="primary" />
+              <Typography
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}>
+                40 porções
+              </Typography>
+            </Stack>
+            <Divider
+              orientation="vertical"
+              sx={{ borderColor: "primary.light" }}
+              flexItem
+            />
+            <Stack
+              direction="row"
+              spacing={0.5}
+              sx={{
+                flexWrap: "nowrap",
+                alignItems: "center",
+                justifyContent: "center",
+                minWidth: 0,
+                width: "100%",
+              }}>
+              <PersonOutlineOutlinedIcon fontSize="small" color="primary" />
+              <Typography
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}>
+                Beatriz Cerqueira
+              </Typography>
+            </Stack>
+            <Divider
+              orientation="vertical"
+              sx={{ borderColor: "primary.light" }}
+              flexItem
+            />
+            <Stack
+              direction="row"
+              spacing={0.5}
+              sx={{
+                flexWrap: "nowrap",
+                alignItems: "center",
+                justifyContent: "center",
+                minWidth: 0,
+                width: "100%",
+              }}>
+              <CalendarMonthOutlinedIcon fontSize="small" color="primary" />
+              <Typography
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}>
+                06/06/2023
+              </Typography>
+            </Stack>
           </Stack>
-
           <Typography variant="h5">Ingredientes</Typography>
         </Card>
       </Grid>
