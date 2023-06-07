80b1caf7f816a846deb8e994479376ed016522d7
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2023-06-06 23:18:06 -0300

    icone delete

--- src/pages/RecipeCreation/components/RecipeCreationForm.tsx
+++ src/pages/RecipeCreation/components/RecipeCreationForm.tsx
@@ -1,11 +1,13 @@
 import * as Yup from "yup";
+import Grid from "@mui/material/Grid";
+
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Formik, Form, Field, FormikHelpers } from "formik";
 import { TextField } from "formik-mui";
 import FormErrorMessages from "../../../enums/errorMessages";
 import { useState } from "react";
-
+import DeleteIcon from "@mui/icons-material/Delete";
 interface IFields {
   recipeTitle: string;
   ingredients: string[];
@@ -70,7 +72,8 @@ export default function RecipeCreationForm() {
     <Formik
       initialValues={initialValues}
       validationSchema={RecipeCreationSchema}
-      onSubmit={onSubmit}>
+      onSubmit={onSubmit}
+    >
       {({ submitForm, isSubmitting }) => (
         <Form>
           <Field
@@ -81,15 +84,25 @@ export default function RecipeCreationForm() {
             label="Título da Receita"
           />
           <Typography> Ingredientes</Typography>
+
           {ingredients.map((_, index) => (
-            <Field
-              component={TextField}
-              size="small"
-              fullWidth
-              name={`ingredients[${index}]`}
-              type="text"
-            />
+            <Grid container>
+              <Grid item xs={11}>
+                <Field
+                  component={TextField}
+                  size="small"
+                  fullWidth
+                  name={`ingredients[${index}]`}
+                  type="text"
+                />
+              </Grid>
+
+              <Grid item xs>
+                <DeleteIcon fontSize="large" color="error" />
+              </Grid>
+            </Grid>
           ))}
+
           <Button onClick={addIngredient}>Adicionar ingrediente</Button>
           <br />
           <br />
