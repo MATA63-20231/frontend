1bc0760614dde097063a09da0e4cfdaa54552124
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2023-06-06 22:45:00 -0300

    stack

--- src/pages/RecipeView.tsx
+++ src/pages/RecipeView.tsx
@@ -9,6 +9,8 @@ import TimerOutlinedIcon from "@mui/icons-material/TimerOutlined";
 import RamenDiningOutlinedIcon from "@mui/icons-material/RamenDiningOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 export default function RecipeView() {
   return (
@@ -19,94 +21,68 @@ export default function RecipeView() {
             component="img"
             image="https://source.unsplash.com/random?wallpapers"
           />
-          <List
-            sx={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-              justifyItems: "center",
-              flexDirection: {xs: "column", md:"row"},
-              width: "100%",
-            }}
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+            justifyContent="center"
+            alignItems="center"
+            sx={{ minWidth: 0 }}
           >
-            <ListItem
-              sx={{
-                px: 1,
-                width: "min-content",
-                maxWidth:"25%",
-                textOverflow:"ellipsis",
-                overflow:"hidden",
-                whiteSpace:"nowrap"
-              }}
-            >
-              <TimerOutlinedIcon
-                fontSize="small"
-                color="primary"
-                sx={{ mr: 0.5 }}
-              />
-              1h30min
-            </ListItem>
-            <ListItem
-              sx={{
-                px: 1,
-                width: "min-content",
-                maxWidth:"25%",
-                textOverflow:"ellipsis",
-                overflow:"hidden",
-                whiteSpace:"nowrap"
-              }}
-            >
-              <RamenDiningOutlinedIcon
-                fontSize="small"
-                color="primary"
-                sx={{ mr: 0.5 }}
-              />
-              40 porções
-            </ListItem>
-            <ListItem
-              sx={{
-                px: 1,
-                width: "min-content",
-                maxWidth:"25%",
-                textOverflow:"ellipsis",
-                overflow:"hidden",
-                whiteSpace:"nowrap"
-              }}
-            >
-              <PersonOutlineOutlinedIcon
-                fontSize="small"
-                color="primary"
-                sx={{ mr: 0.5 }}
-              />
-              Beatriz Cerqueira
-            </ListItem>
-            <ListItem
-              sx={{
-                px: 1,
-                width: "min-content",
-                maxWidth:"25%",
-                textOverflow:"ellipsis",
-                overflow:"hidden",
-                whiteSpace:"nowrap"
-              }}
-            >
-              <CalendarMonthOutlinedIcon
-                fontSize="small"
-                color="primary"
-                sx={{ mr: 0.5 }}
-              />
-              06/06/2023
-            </ListItem>
-          </List>
+            <Grid item>
+              <Grid container sx={{ flexWrap: "nowrap" }}>
+                <TimerOutlinedIcon
+                  fontSize="small"
+                  color="primary"
+                  sx={{ mr: 0.5 }}
+                />
+                <Typography noWrap sx={{ maxWidth: "100%" }}>
+                  1h30min
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container sx={{ flexWrap: "nowrap" }}>
+                <RamenDiningOutlinedIcon
+                  fontSize="small"
+                  color="primary"
+                  sx={{ mr: 0.5 }}
+                />
+                <Typography noWrap sx={{ maxWidth: "100%" }}>
+                  40 porções
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container sx={{ flexWrap: "nowrap" }}>
+                <PersonOutlineOutlinedIcon
+                  fontSize="small"
+                  color="primary"
+                  sx={{ mr: 0.5 }}
+                />
+                <Typography noWrap sx={{ maxWidth: "100%" }}>
+                  Beatriz Cerqueira
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container sx={{ flexWrap: "nowrap" }}>
+                <CalendarMonthOutlinedIcon
+                  fontSize="small"
+                  color="primary"
+                  sx={{ mr: 0.5 }}
+                />
+                <Typography noWrap sx={{ maxWidth: "100%" }}>
+                  06/06/2023
+                </Typography>
+              </Grid>
+            </Grid>
+          </Stack>
+
+          <Typography variant="h5">Ingredientes</Typography>
         </Card>
       </Grid>
     </Page>
   );
 }
 
-// titulo
 // descricao
-// rendimento
-// tempopreparo
-// imagem
-// dataCadastro
