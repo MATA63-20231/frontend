fdf05936f872440d08f420f29f1217b6400f3107
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-06-22 23:07:22 -0300

    v1

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,10 +1,17 @@
 import axios from "axios";
 import { IRecipe } from "../interfaces/interfaces.tsx";
 
-interface IGet {
-  path: string;
+// interface IGet {
+//   path: string;
+//   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+//   onSuccess: (data) => void;
+//   onError?: () => void;
+//   onFinish?: () => void;
+// }
+
+interface IRequest {
+  baseRequest: any;
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  onSuccess: (data) => void;
   onError?: () => void;
   onFinish?: () => void;
 }
@@ -13,13 +20,25 @@ const api = axios.create({
   baseURL: "https://chef-virtual.onrender.com/",
 });
 
-const GET = ({ path, setLoading, onSuccess, onError, onFinish }: IGet) => {
+// const GET = ({ path, setLoading, onSuccess, onError, onFinish }: IGet) => {
+//   setLoading(true);
+//   api
+//     .get(path)
+//     .then(({ data }) => {
+//       onSuccess(data);
+//     })
+//     .catch(() => {
+//       onError?.();
+//     })
+//     .finally(() => {
+//       onFinish?.();
+//       setLoading(false);
+//     });
+// };
+
+const request = ({ baseRequest, setLoading, onError, onFinish }: IRequest) => {
   setLoading(true);
-  api
-    .get(path)
-    .then(({ data }) => {
-      onSuccess(data);
-    })
+  baseRequest
     .catch(() => {
       onError?.();
     })
@@ -30,10 +49,12 @@ const GET = ({ path, setLoading, onSuccess, onError, onFinish }: IGet) => {
 };
 
 export const getRecipes = (setLoading: any, setRecipes: any) => {
-  GET({
-    path: "/receitas",
+  request({
     setLoading,
-    onSuccess: (data) => setRecipes(data),
+    baseRequest: api.get<IRecipe[]>("/receitas").then(({ data }) => {
+      console.log(data);
+      setRecipes(data);
+    }),
   });
 };
 
