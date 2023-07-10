324e996116992227f0549542c9cec17e6fe3bb52
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-07-09 23:46:40 -0300

    lint fix

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,9 @@
 import {
-  PropsWithChildren, createContext, useEffect, useState,
+  PropsWithChildren,
+  createContext,
+  useEffect,
+  useMemo,
+  useState,
 } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../services/Api.tsx";
@@ -7,7 +11,7 @@ import { api } from "../services/Api.tsx";
 interface AuthContextData {
   signed: boolean;
   handleLogin: (token: string) => void;
-  handleLogout: () => void
+  handleLogout: () => void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -16,6 +20,28 @@ export function AuthProvider({ children }: PropsWithChildren) {
   const navigate = useNavigate();
 
   const [signed, setSigned] = useState<boolean>(false);
+
+  const authContextValue = useMemo(() => {
+    const handleLogin = (token: string) => {
+      setSigned(true);
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+
+      // TODO: Change this ugly ternary when token is returned by backend
+      localStorage.setItem("token", token || "null");
+      // localStorage.setItem("token", token);
+
+      navigate("/");
+    };
+
+    const handleLogout = () => {
+      setSigned(false);
+      api.defaults.headers.Authorization = null;
+      localStorage.clear();
+      navigate("/");
+    };
+
+    return { signed, handleLogin, handleLogout };
+  }, [signed, navigate]);
 
   useEffect(() => {
     const storagedToken = localStorage.getItem("token");
@@ -30,26 +56,8 @@ export function AuthProvider({ children }: PropsWithChildren) {
     }
   }, []);
 
-  const handleLogin = (token: string) => {
-    setSigned(true);
-    api.defaults.headers.Authorization = `Bearer ${token}`;
-
-    // TODO: Change this ugly ternary when token is returned by backend
-    localStorage.setItem("token", token || "null");
-    // localStorage.setItem("token", token);
-
-    navigate("/");
-  };
-
-  const handleLogout = () => {
-    setSigned(false);
-    api.defaults.headers.Authorization = null;
-    localStorage.clear();
-    navigate("/");
-  };
-
   return (
-    <AuthContext.Provider value={{ signed, handleLogin, handleLogout }}>
+    <AuthContext.Provider value={authContextValue}>
       {children}
     </AuthContext.Provider>
   );
