30d688a542351703f7b32697cc179e999c342769
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2023-07-12 19:13:17 -0300

    limpa

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,22 +1,15 @@
 import { IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { getRecipesSearch } from "../../services/RecipesApi";
 import { useState } from "react";
-import { IRecipe } from "../../interfaces/RecipeInterfaces";
 import { useNavigate } from "react-router-dom";
 
-// import SearchBar from "material-ui-search-bar"; //material-ui-search-bar
-
 export default function SearchBarComponent() {
-  const [recipes, setRecipes] = useState<IRecipe[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
   const [value, setValue] = useState<String>("");
 
   const navigate = useNavigate();
 
   const handleSubmit = () => {
     navigate(`/receita/busca/${value}`);
-    getRecipesSearch(value, setLoading, setRecipes);
     setValue("");
   };
 
