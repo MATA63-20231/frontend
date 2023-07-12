22a0cbb1fc6095cbd50f78fd3b35363d6c92207c
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2023-07-12 15:08:01 -0300

    recipe edit images

--- src/pages/Recipe/RecipeEdit.tsx
+++ src/pages/Recipe/RecipeEdit.tsx
@@ -1,3 +1,4 @@
+import { enqueueSnackbar } from "notistack";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
@@ -9,6 +10,7 @@ import {
 import Page from "../../components/Page/Page.tsx";
 import RecipeForm from "./components/RecipeForm.tsx";
 import RouteAuthRules from "../../enums/RouteAuthRules.tsx";
+import env from "../../config/env.tsx";
 
 export default function RecipeEdit() {
   const navigate = useNavigate();
@@ -17,6 +19,15 @@ export default function RecipeEdit() {
   const [loading, setLoading] = useState<boolean>(false);
   const [rawRecipe, setRawRecipe] = useState<IRecipe>();
   const [recipeForm, setRecipeForm] = useState<IRecipeFormFields>();
+
+  async function getFileFromUrl(url: string, name: string) {
+    const defaultType = "image/jpeg";
+    const response = await fetch(url);
+    const data = await response.blob();
+    return new File([data], name, {
+      type: data.type || defaultType,
+    });
+  }
 
   useEffect(() => {
     if (!recipeId) {
@@ -28,21 +39,38 @@ export default function RecipeEdit() {
 
   useEffect(() => {
     if (rawRecipe) {
-      const recipeToForm = (recipe: IRecipe): IRecipeFormFields => ({
-        title: recipe.titulo,
-        description: recipe.descricao,
-        servings: Number(recipe.rendimento),
-        prepTime: {
-          hours: Number(recipe.tempoPreparo.horas),
-          minutes: Number(recipe.tempoPreparo.minutos),
-        },
-        directions: recipe.listaPreparo.map(({ descricao }) => descricao),
-        ingredients: recipe.ingredientes.map(({ descricao }) => descricao),
-        // TODO: images
-        images: [],
-      });
-
-      setRecipeForm(recipeToForm(rawRecipe));
+      const imagesPromises = rawRecipe.imagens.map(({ id, nome }) =>
+        getFileFromUrl(`${env.baseUrl}imagem/${id}`, nome)
+      );
+      setLoading(true);
+      Promise.all(imagesPromises)
+        .then((images) => {
+          const recipeToForm = {
+            title: rawRecipe.titulo,
+            description: rawRecipe.descricao,
+            servings: Number(rawRecipe.rendimento),
+            prepTime: {
+              hours: Number(rawRecipe.tempoPreparo.horas),
+              minutes: Number(rawRecipe.tempoPreparo.minutos),
+            },
+            directions: rawRecipe.listaPreparo.map(
+              ({ descricao }) => descricao
+            ),
+            ingredients: rawRecipe.ingredientes.map(
+              ({ descricao }) => descricao
+            ),
+            images,
+          };
+          setRecipeForm(recipeToForm);
+        })
+        .catch(() =>
+          enqueueSnackbar({
+            variant: "error",
+            message:
+              "Não foi possível processar as imagens. Por favor, tente mais tarde.",
+          })
+        )
+        .finally(() => setLoading(false));
     }
   }, [rawRecipe]);
 
@@ -55,8 +83,7 @@ export default function RecipeEdit() {
         rule: RouteAuthRules.SAME_USER_ONLY,
         userId: rawRecipe?.usuario?.id,
         redirectTo: `/receita/${recipeId}`,
-      }}
-    >
+      }}>
       <Grid sx={{ maxWidth: "sm", margin: "0 auto" }}>
         <RecipeForm initialRecipe={recipeForm} />
       </Grid>
